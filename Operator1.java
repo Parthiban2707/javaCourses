6146061afccf7c37b227a034c31e84a3a0eb6a68
Parthiban2707 <rioparthi27@gmail.com>
2023-07-23 21:11:22 +0530

    unary program

---
+++ Operator1.java
@@ -0,0 +1,9 @@
+public class Operator1 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        System.out.println(a++ + ++a);
+        System.out.println(b++ + b++);
+    }
+
+}
