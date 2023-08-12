aa3d6716d6611771e047e0af71affc7cd959dc36
Parthiban2707 <rioparthi27@gmail.com>
2023-08-12 15:55:22 +0530

    ss

--- Float.java
+++ Float.java
@@ -1,7 +1,8 @@
 public class Float {
     public static void main(String[] args) {
-        float a =14.0f;
+        float a = 14.0f;
+        System.out.println(a);
         System.out.println(a);
     }
-    
+
 }
